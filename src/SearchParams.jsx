3a13e915c7589c18b83e078066fea9f825020545
Kim Bell <krbell4@gmail.com>
2024-11-04 12:57:53 -0500

    add fetchSearch file uses react-query, add comment to SearchParams to refactor useEffect

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,37 +1,44 @@
 import { useState, useEffect } from "react";
 import Results from "./Results";
 import useBreedList from "./useBreedList";
-
+// import fetchSearch from "./fetchSearch";
+/* 
+  from FEM video "Uncontrolled Forms"
+  https://frontendmasters.com/courses/complete-react-v8/uncontrolled-forms/
+  refactors useEffect w/ fetchSearch (func using react-query plugin)
+*/
 const animals = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
-  const [breed, setBreed] = useState("")
-  const [pets, setPets] = useState([])
-  const [breeds] = useBreedList(animal)
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
-  // empty array means only run on initial load
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    // empty array means only run on initial load
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   async function requestPets() {
     const res = await fetch(
-    `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
-  );
-  const json = await res.json();
+      `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const json = await res.json();
 
-  setPets(json.pets);
+    setPets(json.pets);
   }
 
   return (
     <div className="search-params">
-      <form onSubmit={e => {
-        e.preventDefault()
-        requestPets()
-      }}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
